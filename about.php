58f356a2b5ecb9e9d9e41b135c1e19de682e3cdc
jc477792 <44014400+jc477792@users.noreply.github.com>
2018-10-10 15:28:38 +1000

    Add files via upload

---
+++ about.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Guru Nanak Restaurant</title>
+    <link rel="icon" href="img/core-img/favicon.ico">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+   <?php include("header.php"); ?>
+
+    
+    <div class="breadcumb-area bg-img bg-overlay" style="background-image: url(img/bg-img/bg7.jpg);">
+        <div class="container h-100">
+            <div class="row h-100 align-items-center">
+                <div class="col-12">
+                    <div class="breadcumb-text text-center">
+                        <h2>About us</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div><!-- ##### About Area Start ##### -->
+    <section class="about-area section-padding-80">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="section-heading">
+                        <h3>Who we are and what we do?</h3>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <h6 class="sub-heading pb-5">Great Pride!!! Our Restaurant is serving since 1947; It is Located in the mid of the Metro City “Ahmedabad”, One of the biggest City of Gujarat. It is near our highly used transportation service “Ahmedabad Railway Junction”. The Best Part is, serving Pure Vegetarian Special Punjabi Food with package of Fixed Thali and different Varieties of Foods affordable to every person. The Travelers can take huge benefit of our famous Restaurant by ordering their choosy delicious food. There needs of Leisure and food completes here only. </h6>
+
+                    <h5 class="sub-heading pb-5">Restaurant is having AC and Non AC Halls having superior Infrastructure, an ambience which everyone like to feel while eating food and spending time itself or with family. All other conveniences like Buses, Local Shuttle, Taxes are available just outside of our restaurant, anyone can want to go to another place just for exploring our city having big shopping malls, theaters, Riverfront view, Famous Siddi Saiyad ki Jali or for any other purpose can avail such benefit without wasting his/her time.</h5>
+                </div>
+            </div>
+
+            <div class="row align-items-center mt-70">
+                <!-- Single Cool Fact -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-cool-fact">
+                        <img src="img/core-img/salad.png" alt="">
+                        <h3><span class="counter">150</span></h3>
+                        <h6>Amazing dishes</h6>
+                    </div>
+                </div>
+
+                <!-- Single Cool Fact -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-cool-fact">
+                        <img src="img/core-img/hamburger.png" alt="">
+                        <h3><span class="counter">10</span></h3>
+                        <h6>Basmati</h6>
+                    </div>
+                </div>
+
+                <!-- Single Cool Fact -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-cool-fact">
+                        <img src="img/core-img/rib.png" alt="">
+                        <h3><span class="counter">10</span></h3>
+                        <h6>Dal</h6>
+                    </div>
+                </div>
+
+                <!-- Single Cool Fact -->
+                <div class="col-12 col-sm-6 col-lg-3">
+                    <div class="single-cool-fact">
+                        <img src="img/core-img/pancake.png" alt="">
+                        <h3><span class="counter">10</span></h3>
+                        <h6>Tandoor se</h6>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-12">
+                    <img class="mb-70" src="img/bg-img/about.png" alt="">
+                    
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- ##### About Area End ##### -->
+	<?php include("footer.php");?>
+
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <script src="js/bootstrap/popper.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/plugins/plugins.js"></script>
+    <script src="js/active.js"></script>
+
+
+
+    
+</body>
+</html>
