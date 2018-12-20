f64601dfc9216253906f407dd38817ef6d972f5f
Gurjoban <35648483+Gurjoban@users.noreply.github.com>
2018-12-20 16:12:41 +1000

    Add files via upload

---
+++ menu.php
@@ -0,0 +1,26 @@
+
+<div class="sidebar col-md-2 col-sm-1 ">
+								
+		<div class="sidebar-collapse collapse">
+					
+					<div class="nav-sidebar title"><span>Main Menu</span></div>
+					
+							<ul class="nav nav-sidebar">
+						    <li><a href="<?php echo base_url('admin/index'); ?>"><i class="fa fa-bar-chart-o"></i><span class="hidden-sm text">Dashboard</span></a></li>	
+                        
+					
+                        	<li><a href="<?php echo base_url('admin/customer'); ?>"><i class="fa fa-bar-chart-o"></i><span class="hidden-sm text"> Manage User</span></a></li>	
+                          	<li><a href="<?php echo base_url('admin/cat_add'); ?>"><i class="fa fa-bar-chart-o"></i><span class="hidden-sm text">Manage Category</span></a></li>
+						    
+                        	<li><a href="<?php echo base_url('admin/store'); ?>"><i class="fa fa-bar-chart-o"></i><span class="hidden-sm text">Manage Menu</span></a></li>
+                            
+                            <li><a href="<?php echo base_url('admin/store_completed'); ?>"><i class="fa fa-bar-chart-o"></i><span class="hidden-sm text">List Menu</span></a></li>
+                              
+    
+                			</ul>
+					
+					
+		</div>
+</div>
+		
+						
