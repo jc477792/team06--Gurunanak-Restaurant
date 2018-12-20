6c3a7703049abe52b7401fa79dfea2ce78243753
Gurjoban <35648483+Gurjoban@users.noreply.github.com>
2018-12-20 16:22:17 +1000

    Add files via upload

--- admin/application/views/admin/menu.php
+++ admin/application/views/admin/menu.php
@@ -10,12 +10,12 @@
                         
 					
                         	<li><a href="<?php echo base_url('admin/customer'); ?>"><i class="fa fa-bar-chart-o"></i><span class="hidden-sm text"> Manage User</span></a></li>	
-                          	<li><a href="#"><i class="fa fa-bar-chart-o"></i><span class="hidden-sm text">Manage Category</span></a></li>
+                          	<li><a href="<?php echo base_url('admin/cat_add'); ?>"><i class="fa fa-bar-chart-o"></i><span class="hidden-sm text">Manage Category</span></a></li>
 						    
-                        	<li><a href="#"><i class="fa fa-bar-chart-o"></i><span class="hidden-sm text">Manage Menu</span></a></li>
+                        	<li><a href="<?php echo base_url('admin/store'); ?>"><i class="fa fa-bar-chart-o"></i><span class="hidden-sm text">Manage Menu</span></a></li>
                             
-                            <li><a href="#"><i class="fa fa-bar-chart-o"></i><span class="hidden-sm text">List Menu</span></a></li>
-                              <li><a href="#"><i class="fa fa-bar-chart-o"></i><span class="hidden-sm text"> Manage Latest Deals</span></a></li>
+                            <li><a href="<?php echo base_url('admin/store_completed'); ?>"><i class="fa fa-bar-chart-o"></i><span class="hidden-sm text">List Menu</span></a></li>
+                              
     
                 			</ul>
 					
