0b90b1e2812539a7f5d4c43fe2d6a3aa9e4a4b4b
rajandeep1994 <45810390+rajandeep1994@users.noreply.github.com>
2018-12-20 15:58:38 +1000

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,16 +1,24 @@
-<footer class="footer-area" style="    background-color: #000;color:#FFF">
+
+ <footer class="footer-area" style="    background-color: #000;color:#FFF">
   <div class="container h-100">
     <div class="row h-100">
-      <div class="col-12 h-100 d-flex flex-wrap align-items-center justify-content-between"> 
+      <div class="col-12 col-xs-12 col-md-12 col-sm-12 h-100"> 
         <!-- Footer Social Info -->
-        <div class="text-right"> Email Us : <a href="mailto:gurunanak@gmail.com">gurunanak@gmail.com</a> || Call Us : <a href="tel:91 7922134734">91 7922134734</a> </div>
+        <div class="footer-info col-xs-12 col-sm-12"> 
+               Email Us : <a href="mailto:gurunanak@gmail.com">gurunanak@gmail.com</a> || Call Us : <a href="tel:91 7922134734">91 7922134734</a></div>
+			
         <!-- Footer Logo -->
-        <div class="footer-logo"> <a href="index.php"><img src="img/core-img/logo.png" alt="" style="height:50px;"></a> </div>
-        <!-- Copyright -->
-        <p>
-          Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Guru Nanak Restaurant</a> 
-          </p>
+        <div class="footer-logo col-sm-12 col-xs-12"> <a href="index.php"><img src="img/core-img/logo.png" alt="" style="height:50px;"></a> </div>
+        <!--  Terms and Copyright -->
+        <div class="col-xs-12 col-sm-12">  
+        <a class="term-condition-link" href="termsandcondition.php">Terms & Condition</a>
+		  </div>
       </div>
     </div>
+  </div>
+  <div class="footer-bottom">
+  	<div class="container">
+  		<p>Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | Guru Nanak Restaurant</p>
+  	</div>
   </div>
 </footer>
