8949eebb8796099d76d77f0e68bfbbd529aa04a6
Gurjoban <35648483+Gurjoban@users.noreply.github.com>
2018-12-20 16:18:37 +1000

    Add files via upload

--- index.php
+++ index.php
@@ -1,316 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-<head>
-    <meta charset="UTF-8">
-    <meta name="description" content="">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Guru Nanak Restaurant</title>
-    <link rel="icon" href="img/core-img/favicon.ico">
-    <link rel="stylesheet" href="style.css">
-</head>
+<?php $this->load->view('admin/header') ?>
+<?php $this->load->view('admin/menu'); ?>
 
-<body>
-   
-   <?php include("header.php"); ?>
-
-    <!-- Slider -->
-    <section class="hero-area">
-        <div class="hero-slides owl-carousel">
-            
-            <div class="single-hero-slide bg-img" style="background-image: url(img/bg-img/bg1.jpg);">
-                <div class="container h-100">
-                    <div class="row h-100 align-items-center">
-                        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-                            <div class="hero-slides-content" data-animation="fadeInDown" data-delay="100ms">
-                                <h2 data-animation="fadeInDown" data-delay="300ms">Delicious Punjabi Food</h2>
-                                <p data-animation="fadeInDown" data-delay="700ms">Punjabi cuisine is a culinary style originating in the punjab, a region in the northern part of the Indian subcontinent. Punjabi food is well known for its enriched flavours of the milk products like paneer, mawa, whitebutter. Main dishes are "Sarso ka Saag" and "Maki di Roti", Chhole bhature, Malai kofta, Punjabi kadi and so many variety of creative dishes.</p>
-                                 </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            
-            <div class="single-hero-slide bg-img" style="background-image: url(img/bg-img/bg6.jpg);">
-                <div class="container h-100">
-                    <div class="row h-100 align-items-center">
-                        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-                            <div class="hero-slides-content" data-animation="fadeInDown" data-delay="100ms">
-                                <h2 data-animation="fadeInDown" data-delay="300ms">Delicious Punjabi Food</h2>
-                                <p data-animation="fadeInDown" data-delay="700ms">Punjabi cuisine is a culinary style originating in the punjab, a region in the northern part of the Indian subcontinent. Punjabi food is well known for its enriched flavours of the milk products like paneer, mawa, whitebutter. Main dishes are "Sarso ka Saag" and "Maki di Roti", Chhole bhature, Malai kofta, Punjabi kadi and so many variety of creative dishes.</P>
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            
-            <div class="single-hero-slide bg-img" style="background-image: url(img/bg-img/bg7.jpg);">
-                <div class="container h-100">
-                    <div class="row h-100 align-items-center">
-                        <div class="col-12 col-md-9 col-lg-7 col-xl-6">
-                            <div class="hero-slides-content" data-animation="fadeInUp" data-delay="100ms">
-                                <h2 data-animation="fadeInUp" data-delay="300ms">Delicious Punjabi Food</h2>
-                                <p data-animation="fadeInUp" data-delay="700ms">Punjabi cuisine is a culinary style originating in the punjab, a region in the northern part of the Indian subcontinent. Punjabi food is well known for its enriched flavours of the milk products like paneer, mawa, whitebutter. Main dishes are "Sarso ka Saag" and "Maki di Roti", Chhole bhature, Malai kofta, Punjabi kadi and so many variety of creative dishes.</P>
-                                
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+<div class="col-md-10 col-sm-11 main ">
+  <div class="row">
+    <div class="col-lg-12">
+      <div class="panel panel-default">
+        <div class="panel-heading">
+          <h2><i class="fa fa-asterisk"></i><span class="break"></span>Quick Links</h2>
         </div>
-    </section>
-    <!-- Slider End -->
-
-    <!-- Catagory  -->
-    <section class="top-catagory-area section-padding-80-0">
-        <div class="container">
-            <div class="row">
-                <!-- Top Catagory Area -->
-                <div class="col-12 col-lg-6">
-                    <div class="single-top-catagory">
-                        <img src="img/bg-img/bg2.jpg" alt="">
-                        <!-- Content -->
-                        <div class="top-cta-content">
-                            <h3>Patiala Lassi </h3>
-                            <h6>Simple &amp; Delicios</h6>
-                            
-                        </div>
-                    </div>
-                </div>
-                <!-- Top Catagory Area -->
-                <div class="col-12 col-lg-6">
-                    <div class="single-top-catagory">
-                        <img src="img/bg-img/bg3.jpg" alt="">
-                        <!-- Content -->
-                        <div class="top-cta-content">
-                            <h3>Aloo Paratha</h3>
-                            <h6>Simple &amp; Delicios</h6>
-                            
-                        </div>
-                    </div>
-                </div>
-            </div>
+        <div class="panel-body">
+          <div class="col-sm-3"> <a href="<?php echo base_url('admin/customer'); ?>" class="quick-button"> <i class="fa fa-group"></i>
+            <p>Manage Users</p>
+            </a> </div>
+          <div class="col-sm-3"> <a href="<?php echo base_url('admin/cat_list'); ?>" class="quick-button"> <i class="fa fa-comments-o"></i>
+            <p>Categories</p>
+            </a> </div>
+          <div class="col-sm-3"> <a href="<?php echo base_url('admin/store'); ?>" class="quick-button"> <i class="fa fa-list"></i>
+            <p>Manage Menu</p>
+            </a> </div>
+          <div class="col-sm-3"> <a href="" class="quick-button"> <i class="fa fa-group"></i>
+            <p>Manage Latest Deals</p>
+            </a> </div>
+          
+          
+          <div class="clearfix"></div>
         </div>
-    </section>
-    <!-- Catagory End  -->
-
-    <!--  Best Dishes -->
-    <section class="best-receipe-area">
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="section-heading">
-                        <h3>The best Dishes</h3>
-                    </div>
-                </div>
-            </div>
-
-            <div class="row">
-                
-                <div class="col-12 col-sm-6 col-lg-4">
-                    <div class="single-best-receipe-area mb-30">
-                        <img src="img/bg-img/r1.jpg" alt="">
-                        <div class="receipe-content">
-                            <a href="receipe-post.php">
-                                <h5>Veg Biriyani</h5>
-                            </a>
-                            <div class="ratings">
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star-o" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                
-                <div class="col-12 col-sm-6 col-lg-4">
-                    <div class="single-best-receipe-area mb-30">
-                        <img src="img/bg-img/r2.jpg" alt="">
-                        <div class="receipe-content">
-                            <a href="receipe-post.php">
-                                <h5>Palak Paneer</h5>
-                            </a>
-                            <div class="ratings">
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star-o" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                
-                <div class="col-12 col-sm-6 col-lg-4">
-                    <div class="single-best-receipe-area mb-30">
-                        <img src="img/bg-img/r3.jpg" alt="">
-                        <div class="receipe-content">
-                            <a href="receipe-post.php">
-                                <h5>Mix Vegetable</h5>
-                            </a>
-                            <div class="ratings">
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star-o" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                
-                <div class="col-12 col-sm-6 col-lg-4">
-                    <div class="single-best-receipe-area mb-30">
-                        <img src="img/bg-img/r4.jpg" alt="">
-                        <div class="receipe-content">
-                            <a href="receipe-post.php">
-                                <h5>Stuffed Paratha</h5>
-                            </a>
-                            <div class="ratings">
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star-o" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                
-                <div class="col-12 col-sm-6 col-lg-4">
-                    <div class="single-best-receipe-area mb-30">
-                        <img src="img/bg-img/r5.jpg" alt="">
-                        <div class="receipe-content">
-                            <a href="receipe-post.php">
-                                <h5>Harabhara Kabab</h5>
-                            </a>
-                            <div class="ratings">
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star-o" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-                
-                <div class="col-12 col-sm-6 col-lg-4">
-                    <div class="single-best-receipe-area mb-30">
-                        <img src="img/bg-img/r6.jpg" alt="">
-                        <div class="receipe-content">
-                            <a href="receipe-post.php">
-								<h5> Dry Paneer Chilly</h5>
-                            </a>
-                            <div class="ratings">
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star" aria-hidden="true"></i>
-                                <i class="fa fa-star-o" aria-hidden="true"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
-    <!-- Best Dishes End ##### -->
-
-    
- 
-   
-    <!-- Follow Us Instagram  -->
-    <div class="follow-us-instagram">
-        
-        <
-        <div class="insta-feeds d-flex flex-wrap">
-            
-            <div class="single-insta-feeds">
-                <img src="img/bg-img/bg1.jpg" alt="" style="height:193px;width:193px;">
-                <!-- Instagram icon -->
-                <div class="insta-icon">
-                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                </div>
-            </div>
-
-            
-            <div class="single-insta-feeds">
-                <img src="img/bg-img/bg2.jpg" alt="" style="height:193px;width:193px;">
-                <!-- Icon -->
-                <div class="insta-icon">
-                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                </div>
-            </div>
-
-            
-            <div class="single-insta-feeds">
-                <img src="img/bg-img/bg6.jpg" alt="" style="height:193px;width:193px;">
-                <!-- Icon -->
-                <div class="insta-icon">
-                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                </div>
-            </div>
-
-            
-            <div class="single-insta-feeds">
-                <img src="img/bg-img/r1.jpg" alt="" style="height:193px;width:193px;">
-                <!-- Icon -->
-                <div class="insta-icon">
-                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                </div>
-            </div>
-
-            <!-- Single Insta Feeds -->
-            <div class="single-insta-feeds">
-                <img src="img/bg-img/r4.jpg" alt="" style="height:193px;width:193px;">
-                <!-- Icon -->
-                <div class="insta-icon">
-                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                </div>
-            </div>
-
-            
-            <div class="single-insta-feeds">
-               <img src="img/bg-img/r3.jpg" alt="" style="height:193px;width:193px;">
-                <!-- Icon -->
-                <div class="insta-icon">
-                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                </div>
-            </div>
-
-            
-            <div class="single-insta-feeds">
-                <img src="img/bg-img/r2.jpg" alt="" style="height:193px;width:193px;">
-                <!-- Icon -->
-                <div class="insta-icon">
-                    <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
-                </div>
-            </div>
-        </div>
+      </div>
     </div>
-    <!--  Instagram End ##### -->
-
-    <!--  Footer  -->
-    <?php include("footer.php");?>
-    <script src="js/jquery/jquery-2.2.4.min.js"></script>
-    <script src="js/bootstrap/popper.min.js"></script>
-    <script src="js/bootstrap/bootstrap.min.js"></script>
-    <script src="js/plugins/plugins.js"></script>
-    <script src="js/active.js"></script>
-
-
+    <!--/col--> 
     
-</body>
-</html>
+  </div>
+</div>
+
