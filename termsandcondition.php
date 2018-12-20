0ff657b8319c481886ae81c5c3abdc5e0703af6a
rajandeep1994 <45810390+rajandeep1994@users.noreply.github.com>
2018-12-20 15:55:53 +1000

    Add files via upload

---
+++ termsandcondition.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+    <meta charset="UTF-8">
+    <meta name="description" content="">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title>Guru Nanak Restaurant</title>
+    <link rel="icon" href="img/core-img/favicon.ico">
+    <link rel="stylesheet" href="style.css">
+
+</head>
+
+<body>
+   <?php include("header.php"); ?>
+    
+    <div class="middle-content">
+    	<div class="container">
+    		<h2>Terms and Conditions</h2>
+    		<p>This site is owned and operated by Guru Nanak Restaurant by using GuruNanakRestaurant.com you agree to be legally bound by these terms, which shall take effect immediately on your first use of GuruNanakRestaurant.com. If you do not agree to be legally bound by all the following terms please do not access and/or use GuruNanakRestaurant.com.</p>
+    		
+    		<p>No material from the site may be copied, reproduced, republished, uploaded, posed, transmitted, or distributed in any way, except that you may download one copy of the materials on any single computer for your non-commercial use only, provided that you keep intact all copyright and other proprietary notices. Modification of the materials or use of the materials for any other purpose is a violation of GuruNanakRestaurant.com. copyright and other proprietary rights. The use of any such material on any other web site or networked computer environment is prohibited. Except as otherwise indicated on this site and except for the trademarks, service marks and trade names of other companies that are displayed on this site, all trademarks, service marks and trade names are property of GuruNanakRestaurant.com.</p>
+    		
+    		<p>GuruNanakRestaurant.com may change these terms at any time by posting changes online. Please review these terms regularly to ensure you are aware of any changes made by GuruNanakRestaurant.com.</p>
+    		
+    		<p>Your continued use of GuruNanakRestaurant.com after changes are posted means you agree to be legally bound by these terms as updated and amended.</p>
+    		
+    		<h2>Promotions and Discounts</h2>
+    		<p>When submitting your order details you may wish to offer to purchase the items specified in conjunction with a particular offer or promotion. To take advantage of this facility you will need to enter the appropriate promo code with your order details. These codes are presented on our menu and other items of promotional literature from time to time.</p>
+    		
+    		<P>Any such order will be subject to the specific terms and conditions of the promotion in question and will be subject to availability. We reserve the right to refuse or withdraw any GuruNanakRestaurant.com promotion or discount at any time.</P>
+    		
+    		<h2>Acceptance of Order</h2>
+    		<p>We reserve the right to accept or reject your offer for any reason, such as the unavailability of any product or service, an error in the price of the product or service or an error in your order. As there may be reasons why we cannot fulfil your order, your contract with us will only come into existence when we advise you that we have accepted your order and receipt of your payment has been confirmed.</p>
+    	</div>
+    </div>    
+    
+	
+	<?php include("footer.php");?>
+
+    <script src="js/jquery/jquery-2.2.4.min.js"></script>
+    <script src="js/bootstrap/popper.min.js"></script>
+    <script src="js/bootstrap/bootstrap.min.js"></script>
+    <script src="js/plugins/plugins.js"></script>
+    <script src="js/active.js"></script>
+
+
+
+    
+</body>
+</html>
